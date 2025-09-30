29e8d48b9040b862e44cbfb45a5a2b3b343bfef2
RetsardCo <235238953+rmm-biscast-student@users.noreply.github.com>
2025-09-30 15:01:25 +0800

    -initial

---
+++ Assets/test2.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test2 : MonoBehaviour
+{
+    public test Test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Test.score = 10;
+
+        test.heart = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
